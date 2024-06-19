14ede4e4082fec218fc3efe54595f95e2250f2c1
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-06-19 21:48:29 +1000

    feat!: refactored into files and added dotenv

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+require(`dotenv`).config();
+// The above is the equivalent of the next two lines:
+// const dotenv = require(`dotenv`);
+// dotenv.config();
+const pokemonPrinterFile = require("./pokemonPrinter");
+
+
+// import blahblah from `dotenv`;
+
+console.log(process.env.ENVIRONMENT_MESSAGE);
+
+
+
 console.log("Terminal app is ruinning!")
 
-const pokemon = require(`pokemon`);
 
-let randomPokemon = pokemon.random();
-console.log(`Today's lucky Pokemon is ${randomPokemon}`);
+
+pokemonPrinterFile.pokemonPrinter();
 
 console.log("Bye bye, terminal app is finished!")
-
